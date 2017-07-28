d7635674243e19aafd386b0c40829496f28d3176
Josh Green <joshuagreen118@gmail.com>
2017-07-28 15:38:20 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+	<head>
+		<title><?php echo get_bloginfo('name'); ?></title>
+		<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<?php if(is_user_logged_in()){ ?>
+			<style>body{margin-top:40px;}</style>
+		<?php } ?>
+		<header>
+			<h3>Logo goes here!</h3>
+			<?php wp_nav_menu(); ?>
+		</header>
+
+		<h2>GIF GOES HERE!</h2>
+		<?php
+		for($i=0;$i<4;$i++){ 
+			echo '<h1>Site under construction!</h1>';
+		}
+		?>
+
+		<?php get_footer(); ?>
+	</body>
+
+</html>
